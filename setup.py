723887a0bab0b196b86a015f81576f2021f71722
Confusion7 <alaudinsmile4444@gmail.com>
2025-03-03 10:12:59 +0530

    updated glorot initializer

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tf-titans",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["tensorflow>=2.11.0"],
     author="Mohammed Saajid",
